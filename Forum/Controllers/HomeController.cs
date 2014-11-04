a11467c9a5847f2a7cb8714b2770782cb582b916
aurelienMerzeau17 <aurelien.merzeau@gmail.com>
2014-11-04 19:00:11 +0100

    bla

--- Forum/Controllers/HomeController.cs
+++ Forum/Controllers/HomeController.cs
@@ -9,21 +9,9 @@ namespace Forum.Controllers
 {
     public class HomeController : Controller
     {
-        string connexionstring = "data source=avip9np4yy.database.windows.net,1433;initial catalog=YoupDEV;persist security info=True;user id=youpDEV;password=youpD3VASP*;MultipleActiveResultSets=True;App=EntityFramework";
-        SqlConnection myConnection;
         
         public ActionResult Index()
         {
-            myConnection = new SqlConnection(connexionstring);
-            ViewBag.Title = "Home Page";
-            try
-            {
-                myConnection.Open();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
             return View();
         }
     }
